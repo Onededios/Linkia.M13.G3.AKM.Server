a017f7410d74131d467ec5011c102f7b5edf0f4e
Joel <joel10olor@gmail.com>
2024-03-09 15:20:44 +0100

    Updated .gitignore

--- AKM.Server.Core.Extensions/obj/Debug/net8.0/AKM.Server.Core.Extensions.AssemblyInfo.cs
+++ AKM.Server.Core.Extensions/obj/Debug/net8.0/AKM.Server.Core.Extensions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AKM.Server.Core.Extensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bc6714339cc37ab597d395b5c88ea323bb6b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9997262cff0d12fae0d68bfceb0060c281206e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AKM.Server.Core.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AKM.Server.Core.Extensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
