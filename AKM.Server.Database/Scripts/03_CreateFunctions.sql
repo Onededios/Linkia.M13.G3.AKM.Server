d71d53bf071fcf992e48d1bbed832a210b914b83
Manuel Prado <manguel.corocotta@gmail.com>
2024-05-30 11:01:19 +0200

    sql scritps

--- AKM.Server.Database/Scripts/03_CreateFunctions.sql
+++ AKM.Server.Database/Scripts/03_CreateFunctions.sql
@@ -1,15 +1,24 @@
-CREATE OR REPLACE FUNCTION setDateUpdate()
+CREATE OR REPLACE FUNCTION setDateCreation()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.date_update := NOW();
+    NEW.creation_date := NOW();
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION setDateCreation()
+CREATE OR REPLACE FUNCTION setDateUpdate()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.date_creation := NOW();
+    NEW.update_date := NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION save_user_history()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO history_users (user_id, username, email, created_at, updated_at, change_timestamp)
+    VALUES (OLD.user_id, OLD.username, OLD.email, OLD.created_at, OLD.updated_at, NOW());
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
